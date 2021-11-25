aeaeccc42353b1b148705391ae38f3155d3af19b
Lukas8586 <83493516+Lukas8586@users.noreply.github.com>
2021-11-25 17:56:20 +0100

    Create list.py

---
+++ list.py
@@ -0,0 +1,15 @@
+import os
+List1 = [8, 9, 3, 6, 1, 10]
+List1.reverse()
+print("Odwrócona koleność listy jest", List1)
+
+List2 = [91,53, 120, 23, 332,32, 53,64,84]
+List2.sort()
+print("posortowane", List2)
+
+List3 = []
+List3 = List1.copy()
+print("List3 =", List3)
+
+indexvalue = List[2:6]
+print("wartości:", indexvalue)
